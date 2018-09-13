1b566d02070b282599d29a30d4776a8a6f1f7e5a
Ayabonga <32756115+Ayabonga2017@users.noreply.github.com>
2018-09-13 17:03:38 +0100

    added greeted handlebars to display my greeted names and counter

--- greet.js
+++ greet.js
@@ -1,81 +1,38 @@
 module.exports = function (pool) {
 
-var NameStorage ={};
   var GreetingMessage = "";
-  var Countkeep = NameStorage || {};
-  var person="";
-var language ="";
 
-  function setperson(value) {
-    if (value !== "") {
-    return person;
+  async function GreetLanguage(language, person) {
+
+    let result = await pool.query('select * from users where names = $1', [person])
+    if (result.rowCount === 0) {
+      await pool.query('insert into users(names, counts) values($1, $2)', [person, 1])
+    } else {
+      await pool.query('update users set counts=counts+1 where names =$1', [person])
     }
-
+    if (language === "English") { GreetingMessage = "Hey, " + person; }
+    if (language === "IsiXhosa") { GreetingMessage = "Mhollo, " + person; }
+    if (language === "Afrikaans") { GreetingMessage = "Halo, " + person; console.log(language); }
   }
-
-  function GreetLanguage(language,person) {
-
-
-    if (Countkeep[person] === undefined) {
-
-      Countkeep[person] = 0;
-    }
-
-    if (language === "English") {
-
-      GreetingMessage = "Hey, " + person;
-
-    }
-    if (language === "IsiXhosa") {
-
-      GreetingMessage = "Mhollo, " + person;
-
-    }
-    if (language === "Afrikaans") {
-
-      GreetingMessage = "Halo, " + person;
-      console.log(language);
-
-    }
-
-  }
-
-  async function addNameToDB(name){
-
-    await pool.query('insert into users(names, counts) values($1, $2)',[name,0])
-
-  }
-
-  function setlang(value) {language = value;}
-  function LanguageReturn() { return language; }
-  function PersonReturn() { return Countkeep; }
-  function Message() { return GreetingMessage;}
-  async function Counter() { 
-    let counter = await pool.query('select count(*) from users')
-    // console.log(counter.rows[0].count)
+  function setlang(value) { language = value; }
+  function Message() { return GreetingMessage; }
+  async function Counter() {
+    var counter = await pool.query('select count(*) from users')
     return counter.rows[0].count
   }
-  function resetBtn() {
-
-     NameStorage ={};
-     GreetingMessage = "";
-     Countkeep = 0;
-     person="";
-     language ="";
+  async function greetedNames() {
+    var counter = await pool.query('select * from users  ')
+    return counter.rows;  
   }
-
+  function resetBtn() {
+    counter = 0;
+  }
   return {
-
     GreetLanguage,
     Message,
     Counter,
-    LanguageReturn,
-    PersonReturn,
     resetBtn,
-    setperson,
-    addNameToDB,
+    greetedNames,
     setlang
-
   }
-
 }
--- index.js
+++ index.js
@@ -3,7 +3,18 @@ let app = express();
 const exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 const Greet = require('./greet');
+const flash = require('express-flash');
+const session = require('express-session');
 
+// initialise session middleware - flash-express depends on it
+app.use(session({
+  secret : "<add a secret string here>",
+  resave: false,
+  saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
 app.use(express.static('public'));
 app.set('view engine', 'handlebars');
 app.engine('handlebars', exphbs({
@@ -15,82 +26,57 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }))
-
-
 const pg = require("pg");
 const Pool = pg.Pool;
-
-
-
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
-    useSSL = true;
+if (process.env.DATABASE_URL && !local) {
+  useSSL = true;
 }
 // which db connection to use
 const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:mlandeli2017@localhost:5555/greetings';
-
 const pool = new Pool({
-    connectionString,
-    ssl : useSSL
-  });
-  const factory = Greet(pool);
+  connectionString,
+  ssl: useSSL
+});
+const factory = Greet(pool);
 app.get("/", async function (req, res, next) {
-
-  try  {
-    res.render("home");
+  let counts = await factory.Counter()
+  try {
+    res.render("home", { counts });
   } catch (error) {
     next(error);
   }
 });
-
-app.post('/greetings', async function (req, res) {
-
-  // get the values from the form (req.body)
-  var language = req.body.language;
-  var firstName = req.body.firstName;
-  
-  await factory.addNameToDB(firstName)
-
-  // use the values in the Factory function
-  factory.setlang(language);
-  factory.setperson(firstName);
-  var name_language = factory.GreetLanguage(language, firstName)
-
-  let results = {
-
-    language: factory.LanguageReturn(),
-    firstName: factory.PersonReturn()
+app.post('/greetings', async function (req, res, next) {
+  try {
+    // get the values from the form (req.body)
+    var language = req.body.language;
+    var firstName = req.body.firstName;
+    var name_language = await factory.GreetLanguage(language, firstName)
+    var displaymessage = factory.Message();
+    var counterdisplay = await factory.Counter();
+    console.log(counterdisplay)
+    res.render('home', { name_language, displaymessage, counterdisplay })
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
-  var displaymessage = factory.Message();
-  var counterdisplay= await factory.Counter();
-console.log(counterdisplay)
-  //  let finalCount = counterdisplay.count
-
-  if (firstName === '' && language === undefined) {
-    return displaymessage = 'Please Enter a Name and Select a Language !';
-  }
-  else if (!language) { return displaymessage = "Please select language"; }
-  else if (firstName === "") { return displaymessage = "Please enter name"; }
-
-  res.render('home', {name_language , displaymessage,counterdisplay, results})
 });
-
-app.get('/resets', function(req ,res ){
-
+app.get('/resets', async function (req, res) {  
   var reset = factory.resetBtn();
   console.log(reset);
-  res.render("home", {reset})
+  res.render("home", { reset })
 })
-app.get('/greeted', async function(req ,res ){
-  var fromUsers =  await pool.query("select names from users");
-  let users = fromUsers.rows
+app.get('/greeted', async function (req, res) {
+  
+  let  users = await factory.greetedNames();
+  console.log(users);
+
   res.render("greeted", {
     users
-   });
+  });
 })
 let PORT = process.env.PORT || 1991;
 app.listen(PORT, function () { console.log('App starting on port', PORT); });
-
-
