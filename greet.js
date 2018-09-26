e77b0a7386be1eeee4f21af60b69720e298dca8e
Ayabonga <32756115+Ayabonga2017@users.noreply.github.com>
2018-09-26 09:19:54 +0200

    Update greet.js

--- greet.js
+++ greet.js
@@ -2,34 +2,42 @@ module.exports = function (pool) {
 
   async function GreetLanguage(language, person) {
 
-    let result = await pool.query('select * from users where names = $1', [person])
-    if (result.rows.length === 0) {
-      await pool.query('insert into users(names, counts) values($1, $2)', [person, 1])
-    } else {
-
-      await pool.query("update users set counts=counts+1 where names=$1", [person]);
-    }
-
-    if (language === "English") { return "Hey, " + person; }
-    if (language === "IsiXhosa") { return "Mhollo, " + person; }
-    if (language === "Afrikaans") { return "Halo, " + person; }
+    let result = await pool.query('select * from greetings where names = $1', [person])
+      if (person !=='') { }
+      if (language == undefined) {  }
+      if (result.rows.length === 0) {
+      if (person !== '') { await pool.query('insert into greetings(names, counts) values($1, $2)', [person, 1])}
+  }  else {
+      await pool.query("update greetings set counts=counts+1 where names=$1", [person]);
+    } 
+     if ( person !=='') { } 
+     else {  
+     if (language === "English") { "Hey, " + person; }
+     if (language === "IsiXhosa") { "Mhollo, " + person; }
+     if (language === "Afrikaans") { "Halo, " + person; }
+     }
+      
   }
-
+async function greetedwithL(name){
+  let greeted = await pool.query('select * from greetings where names =$1 ',[name])
+  return  greeted.rows1;
+}
   async function Counter() {
-    var counter = await pool.query('select count(*) from users ')
+    var counter = await pool.query('select count(*) from greetings ')
     return counter.rows[0].count
   }
   async function greetedNames() {
-    var counter = await pool.query('select * from users  ')
+    var counter = await pool.query('select * from greetings  ')
     return counter.rows;
   }
   async function resetBtn() {
-    var reset = await pool.query('delete from users ')
+    var reset = await pool.query('delete from greetings ')
     return reset;
   }
 
   return {
     GreetLanguage,
+    greetedwithL,
     Counter,
     resetBtn,
     greetedNames
