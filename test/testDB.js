ae48991ddce398dd4f8fac0a8f3911620eb1593b
coder <nompetsheniayabonga@gmail.com>
2018-10-01 09:05:32 +0200

    added tests

--- test/testDB.js
+++ test/testDB.js
@@ -1,11 +1,10 @@
 const assert = require('assert');
-const factoryGreet = require('/greet');
-const factory = factoryGreet();
+const Greet = require('../greet');
 const pg = require("pg");
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:coder123@localhost:5432/greetings';
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder123@localhost:5432/greetings_test';
 
 const pool = new Pool({
     connectionString
@@ -15,20 +14,31 @@ describe('The basic database web app', function(){
 
     beforeEach(async function(){
         // clean the tables before each test run
-        await pool.query("delete from greetings;");
-  
+        await pool.query("delete from users;");
     });
 
-    it('should pass the db test', async function(){
-        
-        // the Factory Function is called CategoryService
-        let categoryService = factory(pool);
-        await factory.GreetLanguage.add({
-            description : "Diary"
-        });
+    it('should pass if there\'s no one greeted', async function(){
 
-        let categories = await categoryService.all();
-        assert.equal(1, categories.length);
+        // the Factory Function is called Greet
+        let greetMe = Greet(pool);
+        // let count = await greetMe.greetCounter();
+        assert.equal(0, await greetMe.greetCounter());
+
+    });
+
+    beforeEach(async function(){
+        // clean the tables before each test run
+        await pool.query('delete from users');
+    });
+
+    it('should greet 2 people and the counter is equal to 2', async function(){
+
+        // the Factory Function is called Greet
+        let greetEng = Greet(pool);
+        await greetEng.GreetLanguage("Ayabonga", "English");
+        await GreetLanguageEng.GreetLanguage("Asa", "Afrikaans");
+
+        assert.equal(2, await greetEng.Counter());
 
     });
 
